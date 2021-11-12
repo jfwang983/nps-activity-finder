4533c684760c9f31de79021a1c39e252abe3a4ef
jfwang983 <jfwang983@gmail.com>
2021-11-12 10:20:20 -0800

    Made some aesthetic changes

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -7,13 +7,12 @@ const Frame = (source) => {
         <div>
             <div className="title">
                 <h3>{source.source.title} Webcam</h3>
-                <p>Scroll down in the below frame to see the webcam</p>
+                <p>Scroll through the frame below to see the webcam</p>
             </div>
-            <iframe src={source.source.url} title={source.source.url}></iframe>
             <div className="status">
-                <p>Status:&nbsp;</p>
                 <p className={source.source.status}>{source.source.status}</p>
             </div>
+            <iframe src={source.source.url} title={source.source.url}></iframe>
             <div className="description">
                 <p>Description:</p>
                 <p>{source.source.description}</p>
